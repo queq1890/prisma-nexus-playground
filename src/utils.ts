8aa9912d09faae69a02fa5f2b34262559608352d
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-11-21 11:50:59 +0700

    Remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,7 @@
 import { Context } from "./context";
-import path from "path";
 import { verify } from "jsonwebtoken";
 
-const env = process.env.NODE_ENV;
-const envPath =
-  env === "development"
-    ? path.resolve(__dirname, "../dotenv/dev.env")
-    : env === "test"
-    ? path.resolve(__dirname, "../dotenv/test.env")
-    : path.resolve(__dirname, "../dotenv/.env");
-
-require("dotenv").config({ path: envPath });
-
-const { JWT_SECRET } = process.env;
-export const APP_SECRET = JWT_SECRET;
+export const APP_SECRET = process.env.JWT_SECRET;
 
 interface Token {
   userId: string;
